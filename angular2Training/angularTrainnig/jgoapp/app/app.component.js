36cddc72aeeb4765b8148de11a52d090ff39e0d8
jennifer <jgonzal10@gmail.com>
2016-05-29 15:17:37 -0300

    'developershardcoded'

--- angular2Training/angularTrainnig/jgoapp/app/app.component.js
+++ angular2Training/angularTrainnig/jgoapp/app/app.component.js
@@ -9,15 +9,30 @@ var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
 var core_1 = require('@angular/core');
+var developer_detail_component_1 = require('./developer-detail.component');
+var developer_service_1 = require('./developer.service');
 var AppComponent = (function () {
-    function AppComponent() {
+    function AppComponent(developerService) {
+        this.developerService = developerService;
+        this.title = 'Developers app';
     }
+    AppComponent.prototype.getDevelopers = function () {
+        var _this = this;
+        this.developerService.getDevelopers().then(function (developers) { return _this.developers = developers; });
+    };
+    AppComponent.prototype.ngOnInit = function () {
+        this.getDevelopers();
+    };
+    AppComponent.prototype.onSelect = function (developer) { this.selectedDeveloper = developer; };
     AppComponent = __decorate([
         core_1.Component({
             selector: 'my-app',
-            template: '<h1>JGO with Angularjs 2 Rocks</h1>'
+            template: "\n    <h1>{{title}} by {{powerBy}}</h1>\n    <h2>My Team</h2>\n    <ul class=\"developers\">\n      <li *ngFor=\"let developer of developers\"\n        [class.selected]=\"developer === selectedDeveloper\"\n        (click)=\"onSelect(developer)\">\n        <span class=\"badge\">{{developer.id}}</span> {{developer.name}}\n      </li>\n    </ul>\n    <my-developer-detail [developer]=\"selectedDeveloper\"></my-developer-detail>\n  ",
+            styles: ["\n    .selected {\n      background-color: #CFD8DC !important;\n      color: white;\n    }\n    .developers {\n      margin: 0 0 2em 0;\n      list-style-type: none;\n      padding: 0;\n      width: 15em;\n    }\n    .developers li {\n      cursor: pointer;\n      position: relative;\n      left: 0;\n      background-color: #EEE;\n      margin: .5em;\n      padding: .3em 0;\n      height: 1.6em;\n      border-radius: 4px;\n    }\n    .developers li.selected:hover {\n      background-color: #BBD8DC !important;\n      color: white;\n    }\n    .developers li:hover {\n      color: #607D8B;\n      background-color: #DDD;\n      left: .1em;\n    }\n    .developers .text {\n      position: relative;\n      top: -3px;\n    }\n    .developers .badge {\n      display: inline-block;\n      font-size: small;\n      color: white;\n      padding: 0.8em 0.7em 0 0.7em;\n      background-color: #607D8B;\n      line-height: 1em;\n      position: relative;\n      left: -1px;\n      top: -4px;\n      height: 1.8em;\n      margin-right: .8em;\n      border-radius: 4px 0 0 4px;\n    }\n  "],
+            directives: [developer_detail_component_1.DeveloperDetailComponent],
+            providers: [developer_service_1.DeveloperService]
         }), 
-        __metadata('design:paramtypes', [])
+        __metadata('design:paramtypes', [developer_service_1.DeveloperService])
     ], AppComponent);
     return AppComponent;
 }());
--- angular2Training/angularTrainnig/jgoapp/app/app.component.ts
+++ angular2Training/angularTrainnig/jgoapp/app/app.component.ts
@@ -1,6 +1,90 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { Developer } from './developer';
+import { DeveloperDetailComponent } from './developer-detail.component';
+import { DeveloperService } from './developer.service';
+
 @Component({
   selector: 'my-app',
-  template: '<h1>JGO with Angularjs 2 Rocks</h1>'
+  template:`
+    <h1>{{title}} by {{powerBy}}</h1>
+    <h2>My Team</h2>
+    <ul class="developers">
+      <li *ngFor="let developer of developers"
+        [class.selected]="developer === selectedDeveloper"
+        (click)="onSelect(developer)">
+        <span class="badge">{{developer.id}}</span> {{developer.name}}
+      </li>
+    </ul>
+    <my-developer-detail [developer]="selectedDeveloper"></my-developer-detail>
+  `,
+  styles: [`
+    .selected {
+      background-color: #CFD8DC !important;
+      color: white;
+    }
+    .developers {
+      margin: 0 0 2em 0;
+      list-style-type: none;
+      padding: 0;
+      width: 15em;
+    }
+    .developers li {
+      cursor: pointer;
+      position: relative;
+      left: 0;
+      background-color: #EEE;
+      margin: .5em;
+      padding: .3em 0;
+      height: 1.6em;
+      border-radius: 4px;
+    }
+    .developers li.selected:hover {
+      background-color: #BBD8DC !important;
+      color: white;
+    }
+    .developers li:hover {
+      color: #607D8B;
+      background-color: #DDD;
+      left: .1em;
+    }
+    .developers .text {
+      position: relative;
+      top: -3px;
+    }
+    .developers .badge {
+      display: inline-block;
+      font-size: small;
+      color: white;
+      padding: 0.8em 0.7em 0 0.7em;
+      background-color: #607D8B;
+      line-height: 1em;
+      position: relative;
+      left: -1px;
+      top: -4px;
+      height: 1.8em;
+      margin-right: .8em;
+      border-radius: 4px 0 0 4px;
+    }
+  `],
+  directives: [DeveloperDetailComponent],
+  providers: [DeveloperService]
 })
-export class AppComponent { }
+export class AppComponent implements OnInit {
+  title = 'Developers app';
+  developers: Developer[];
+  selectedDeveloper: Developer;
+  powerBy:'JGO';
+
+  constructor(private developerService: DeveloperService) { }
+
+  getDevelopers() {
+    this.developerService.getDevelopers().then(developers => this.developers = developers);
+  }
+
+  ngOnInit() {
+    this.getDevelopers();
+  }
+
+  onSelect(developer: Developer) { this.selectedDeveloper = developer; }
+}
